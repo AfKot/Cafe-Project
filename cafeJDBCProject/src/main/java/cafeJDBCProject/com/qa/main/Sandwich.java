cfd2c81d00483c89559b02e3bc6036ff214e5849
AfKot <kotadia.afzal@gmail.com>
2022-05-19 11:16:25 +0100

    updated pom file

---
+++ cafeJDBCProject/src/main/java/cafeJDBCProject/com/qa/main/Sandwich.java
@@ -0,0 +1,11 @@
+package cafeJDBCProject.com.qa.main;
+
+public class Sandwich {
+	
+	private int id;
+	private String type;
+	private String size;
+	private boolean glutenFree;
+	private float price;
+
+}
